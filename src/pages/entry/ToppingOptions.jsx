34761d2a082b51791cee6768e25d37b07ebffe7e
Adam Bokor <adam.bokor@mmsone.com>
2022-06-01 14:41:59 +0200

    Toppings counts

--- src/pages/entry/ToppingOptions.jsx
+++ src/pages/entry/ToppingOptions.jsx
@@ -1,6 +1,7 @@
+import { Form } from "react-bootstrap";
 import { Col } from "react-bootstrap";
 
-export default function ToppingOption({ item }) {
+export default function ToppingOption({ item, updateItemCount }) {
   const { name, imagePath } = item;
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
@@ -9,6 +10,15 @@ export default function ToppingOption({ item }) {
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name}_topping`}
       />
+      <Form.Group controlId={`${name}-count`}>
+        <Form.Check
+          type="checkbox"
+          label={name}
+          onChange={(e) => {
+            updateItemCount(name, e.target.checked ? 1 : 0);
+          }}
+        />
+      </Form.Group>
     </Col>
   );
 }
--- src/pages/entry/test/totalUpdates.test.jsx
+++ src/pages/entry/test/totalUpdates.test.jsx
@@ -26,3 +26,28 @@ test("update scoop subtotal when scoops change", async () => {
   await userEvent.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("update topping subtotal when scopps change", async () => {
+  render(<Options optionType="toppings" />);
+  // default subtotal is $0.00
+  const subtotalText = screen.getByText("Toppings total: $", { exact: false });
+  expect(subtotalText).toHaveTextContent("0.00");
+
+  // find and check a box for one option and check subtotal
+  const cherryCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherry",
+  });
+  await userEvent.click(cherryCheckbox);
+  expect(subtotalText).toHaveTextContent("1.50");
+
+  // find and check a box for another option and check subtotal
+  const strawberryCheckbox = await screen.findByRole("checkbox", {
+    name: "Strawberry",
+  });
+  await userEvent.click(strawberryCheckbox);
+  expect(subtotalText).toHaveTextContent("3.00");
+
+  // find and check a box for a checked option and check subtotal
+  await userEvent.click(cherryCheckbox);
+  expect(subtotalText).toHaveTextContent("1.50");
+});
